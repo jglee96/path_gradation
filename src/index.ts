b0d2e8903494185cb57b248c0fa7f82bff2deff0
jglee96 <zakklee96@gmail.com>
2022-04-01 00:50:39 +0900

    [chore] webpack 추가
    * 기존 tsc로는 typescript export, require등 귀찮은 작업들이 많음
    * webpack 사용하여 해결함
    * 참고 url(https://riverandeye.tistory.com/entry/1-Webpack-%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-JavaScript-%EB%B2%88%EB%93%A4%EB%A7%81)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-const hello: string = 'Hello, World!';
-console.log(hello);
+import * as paper from 'paper';
+
+paper.setup('');
+
+const heartPath = 'M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z';
+const heartPathItem = paper.PathItem.create(heartPath);
+
+console.log(heartPathItem.className);
